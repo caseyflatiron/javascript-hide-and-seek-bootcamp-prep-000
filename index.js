548c13fc0b24998cde141dc27935710c8f94a8d3
casey bradford <caseyebradford@gmail.com>
2020-04-22 20:42:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  const first = document.querySelector(selector);
+  return first;
+}
 
+function nestedTarget(){
+  const target = document.querySelector('#nested div div div.target');
+  return target;
+}
+
+function deepestChild() {
+  const deepest =  document.querySelector('#grand-node div div div div');
+  return deepest;
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + n).toString();
+  }
+}
